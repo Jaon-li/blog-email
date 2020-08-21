fcc3f286c1470463c160cd762fb133e78f8c8de3
wesley <zerofire.li@qq.com>
2020-08-21 18:38:30 +0800

    Initial commit

---
+++ src/main/java/com/blog/blogemail/BlogEmailApplication.java
@@ -0,0 +1,13 @@
+package com.blog.blogemail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogEmailApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogEmailApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/blog/blogemail/BlogEmailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blog.blogemail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogEmailApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
