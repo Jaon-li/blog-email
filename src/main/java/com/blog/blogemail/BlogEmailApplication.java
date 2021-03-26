b9f2058128d9892c3ad314f034a43e20c3f83e4a
Jaon-li <17717561090li>
2021-03-26 22:49:00 +0800

    nacos提交

--- src/main/java/com/blog/blogemail/BlogEmailApplication.java
+++ src/main/java/com/blog/blogemail/BlogEmailApplication.java
@@ -1,9 +1,13 @@
 package com.blog.blogemail;
 
+import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.blog.bloguser.mapper")
+@NacosPropertySource(dataId = "email-service.yaml", autoRefreshed = true)
 public class BlogEmailApplication {
 
     public static void main(String[] args) {
